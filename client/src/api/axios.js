b64aac1991828ad7c628a9d0a8a3c99206c47813
rey3d <lakshmiprasadredd@gmail.com>
2026-05-01 14:09:55 +0530

    Connecting the frontend to production backend.

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // axios instance with base URL
 const API=axios.create({
-    baseURL:"http://localhost:5000/api",
+     baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 //attach token - before every request
